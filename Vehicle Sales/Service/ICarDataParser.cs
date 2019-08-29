e9769a9c02bb8a0ab26e210549cf71ce1e29b7c0
sharathsa <sharath.chandra542@gmail.com>
2019-08-29 10:06:32 -0400

    Demo

---
+++ Vehicle Sales/Service/ICarDataParser.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Vehicle_Sales.Models;
+
+namespace Vehicle_Sales.Service
+{
+    public interface ICarDataService
+    {
+        List<VehicleModel> TransformCarData(List<string> lines);
+    }
+}
